6a1461b1313e3fb5770240d688e711bb913328a7
ButterOcelot <tyrcat2005@gmail.com>
2026-01-16 17:40:39 -0500

    yall i didnt do shit, gonna do this over the weekend lol

---
+++ Assets/Scripts/Coding Gyms/Scr_Teleporter.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Scr_Teleporter : MonoBehaviour
+{
+    public float duration;
+    public float 
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 teleportPos = Random.RandomRange(;
+
+
+    }
+}
